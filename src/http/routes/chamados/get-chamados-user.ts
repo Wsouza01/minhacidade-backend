b6fdc012135e19fae57162d48716e8a3f83eeb48
Silas Martins <silas.martins2041@gmail.com>
2025-09-03 21:57:53 -0300

    Create get-chamados-user.ts

---
+++ src/http/routes/chamados/get-chamados-user.ts
@@ -0,0 +1,26 @@
+// src/routes/getChamadosUserRoute.ts
+import fastify from "fastify";
+import { db } from "../../db/connection";
+import { schema } from "../../db/schema";
+
+const { FastifyPluginCallback } = fastify;
+
+export const getChamadosUserRoute: FastifyPluginCallback = (app) => {
+  app.get("/chamados/user/:usuarioId", async (request, reply) => {
+    const { usuarioId } = request.params as { usuarioId: string };
+    const chamados = await db
+      .select({
+        id: schema.chamados.cha_id,
+        nome: schema.chamados.cha_nome,
+        departamento: schema.chamados.cha_departamento,
+        status: schema.chamados.cha_data_fechamento, // ajuste conforme seu status
+        mensagem: schema.chamados.cha_descricao,
+        dataCriacao: schema.chamados.cha_data_abertura,
+        prioridade: schema.chamados.cha_prioridade,
+        protocolo: schema.chamados.cha_protocolo,
+      })
+      .from(schema.chamados)
+      .where(schema.chamados.usu_id.eq(usuarioId));
+    reply.send(chamados);
+  });
+};
